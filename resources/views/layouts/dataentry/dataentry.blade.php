db193041d1db7bd28c980490f07ea03d0c4ec33f
Sunday <sunjhen29@yahoo.com>
2017-06-10 12:31:02 +0800

    js

--- resources/views/layouts/dataentry/dataentry.blade.php
+++ resources/views/layouts/dataentry/dataentry.blade.php
@@ -86,37 +86,7 @@
 
 @stack('scripts')
 
-<script>
-    var $jqDate = jQuery('input.aussie_date[type=text]');
-
-    //Bind keyup/keydown to the input
-    $($jqDate).bind('keyup','keydown', function(e){
-        //To accomdate for backspacing, we detect which key was pressed - if backspace, do nothing:
-        if(e.which !== 8) {
-            var numChars = $jqDate.val().length;
-            if(numChars === 2 || numChars === 5){
-                var thisVal = $jqDate.val();
-                thisVal += '/';
-                $jqDate.val(thisVal);
-            }
-        }
-    });
-
-    $(document).on('click', '.delete', function() {
-        $('#delete_id').val($(this).data("id"));
-    });
-
-</script>
-
-
-<script>
-    $(document).ready(function() {
-        //Datemask dd/mm/yyyy
-        $(".ddmmyyy").inputmask("dd/mm/yyyy", {"placeholder": "dd/mm/yyyy"});
-    });
-</script>
-
-
-
+<!-- Saturday Auction -->
+<script src="/js/saturday_auction.js"></script>
 </body>
 </html>
